8a2d06a860f615266357443653dbd0008d6ddfef
Nick <burkettng@appstate.edu>
2021-08-11 00:21:31 -0500

    Implemented a getArray method

---
+++ MultiClient.java
@@ -0,0 +1,39 @@
+/* 
+ * This will be our client class that creates and get the array to be 
+ * summed up. 
+ * 
+ * @author Nicholas Burkett 
+ * @version V1
+ * @date 08/10/21
+ */
+
+ //Need to import the proper fields. 
+ import java.util.Scanner; //Import Scanner 
+ import java.util.ArrayList; //Import ArrayList 
+
+ //Here is the client class that will run everything. 
+ public class MultiClient{
+
+     ArrayList<Integer> arrList = null; 
+     //Main class for the program. 
+     public static void main(String[] args){
+
+         //Need to add some shit
+     }
+
+     //Creating the get array method that will allow an array to be filled for summation. 
+     public ArrayList<Integer> getArray(){
+
+         Scanner newScan = new Scanner(System.in); //Create a Scanner obj. 
+         boolean helperFlag = true; //Create a flag for looping. 
+
+         while(helperFlag){
+
+             System.out.println("Please choose a number to add to the array: "); //Prompt user for input.
+             char nextChar = newScan.nextChar(); //Grab the user input. 
+
+             if(nextChar > ) 
+         }
+     }
+
+ }
